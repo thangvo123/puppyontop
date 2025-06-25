9585f8144e0ded8d497f878cba39361590c9c8c9
thangvo123 <94835684+thangvo123@users.noreply.github.com>
2025-06-25 07:32:24 +0700

    Update puppyontop.lua

--- puppyontop.lua
+++ puppyontop.lua
@@ -1,10 +1,14 @@
-_G.Config = { UserID = "3bf97fad-2915-4197-8a14-81872043402d", discord_id = "1165842656958611546" , Note = "PUPPY", } loadstring(game:HttpGet("https://raw.githubusercontent.com/skadidau/unfazedfree/refs/heads/main/gag"))()
-script_key = getgenv().DuckHub;
+script_key = "sAbEuThKiUuZITyMNOGjlPRKahOgrkIO";
 getgenv().Webhook_URL = ""
 getgenv().DiscordId = ""
 getgenv().PetToNotif = {
     "Dragonfly",
     "Queen Bee",
+    "Disco Bee",
+    "Butterfly",
+    "Raccon",
+    "Mimic Octopus",
 }
+
 
 loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/2edd753538dd4eb421464d33fc62937d.lua"))()
